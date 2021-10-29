5e083948d010710c9d8225dba3fb93f96bac1b31
hakanolgun <kaptanhakanolgun@gmail.com>
2021-10-29 09:22:01 +0300

    components created

---
+++ src/components/otherDays/OtherDays.js
@@ -0,0 +1,7 @@
+export default function OtherDays() {
+    return (
+        <div>
+            others
+        </div>
+    )
+}
--- src/components/weatherContainer/WeatherContainer.js
+++ src/components/weatherContainer/WeatherContainer.js
@@ -1,9 +1,14 @@
+import OtherDays from "../otherDays/OtherDays";
+import Select from "../select/Select";
+import Today from "../today/Today";
 import styles from "./weathercontainer.module.css";
 
 export default function WeatherContainer() {
-    return (
-        <div className={styles.weatherContainer}>
-            weather container
-        </div>
-    )
+  return (
+    <div className={styles.weatherContainer}>
+      <Select />
+      <Today />
+      <OtherDays />
+    </div>
+  );
 }
